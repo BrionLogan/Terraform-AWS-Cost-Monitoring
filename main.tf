a30a7c413a5854bad9027f5e86c7b14c3cb7b3b3
Brion Logan <blogan@Brions-MacBook-Pro.local>
2026-05-30 14:07:24 -0400

    Added AWS monthly budget alerts

--- main.tf
+++ main.tf
@@ -7,3 +7,35 @@ resource "aws_sns_topic_subscription" "email_alert" {
   protocol  = "email"
   endpoint  = var.alert_email
 }
+
+resource "aws_budgets_budget" "monthly_budget" {
+  name         = "monthly-aws-budget"
+  budget_type  = "COST"
+  limit_amount = var.monthly_budget_amount
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  notification {
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 50
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "ACTUAL"
+    subscriber_sns_topic_arns = [aws_sns_topic.billing_alerts.arn]
+  }
+
+  notification {
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 80
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "ACTUAL"
+    subscriber_sns_topic_arns = [aws_sns_topic.billing_alerts.arn]
+  }
+
+  notification {
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 100
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "FORECASTED"
+    subscriber_sns_topic_arns = [aws_sns_topic.billing_alerts.arn]
+  }
+}
